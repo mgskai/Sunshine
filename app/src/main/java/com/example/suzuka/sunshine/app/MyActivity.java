03068b75805f1d8ee78de2af4e60e3ae73afcfa4
mgskai <mgskai@gmail.com>
2014-09-07 17:02:48 -0400

    Update ForecastFragment

--- app/src/main/java/com/example/suzuka/sunshine/app/MyActivity.java
+++ app/src/main/java/com/example/suzuka/sunshine/app/MyActivity.java
@@ -1,19 +1,9 @@
 package com.example.suzuka.sunshine.app;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 
 public class MyActivity extends Activity {
@@ -24,7 +14,7 @@ public class MyActivity extends Activity {
         setContentView(R.layout.activity_my);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -52,34 +42,5 @@ public class MyActivity extends Activity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
-
-            ArrayList<String> listEntry = new ArrayList<String>();
-            listEntry.add("Today-Sunny-88/63");
-            listEntry.add("Tomorrow-Cloudy-86/65");
-            listEntry.add("Wed-Rainy-82/67");
-            listEntry.add("Thurs-Sunny-82/61");
-            listEntry.add("Fri-Foggy-88/68");
-            listEntry.add("Sat-Snowy-79/69");
-
-            ArrayAdapter<String> listEntryAdapter = new ArrayAdapter<String>(
-                    getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    listEntry);
-
-            ListView lv = (ListView)rootView.findViewById(R.id.listview_forecast);
-            lv.setAdapter(listEntryAdapter);
-
-            return rootView;
-        }
-    }
 }
